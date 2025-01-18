126d3bc65fff9317cc0d0398d7fb77dbed0eec18
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-18 16:57:56 +1000

    create @classmethod

--- src/product.py
+++ src/product.py
@@ -11,3 +11,21 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data=None):
+        """Класс-метод для создания нового объекта Product из словаря"""
+        if product_data is None:
+            product_data = {}  # Используем пустой словарь по умолчанию
+
+            # Извлекаем значения из словаря с использованием значений по умолчанию
+        name = product_data.get("name", "")
+        description = product_data.get("description", "")
+        price = product_data.get("price", 0.0)
+        quantity = product_data.get("quantity", 0)
+
+        # Создаем и возвращаем новый экземпляр класса Product
+        return cls(name, description, price, quantity)
+
+
+
