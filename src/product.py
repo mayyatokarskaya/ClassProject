9aa34b070f714fdc31f50f0d9bb6d2d0edb30afe
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-18 17:15:50 +1000

    create getter, setter for price

--- src/product.py
+++ src/product.py
@@ -9,22 +9,33 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self):
+        """Геттер для получения значения цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """Сеттер для установки значения цены с проверкой"""
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
 
     @classmethod
     def new_product(cls, product_data=None):
         """Класс-метод для создания нового объекта Product из словаря"""
         if product_data is None:
-            product_data = {}  # Используем пустой словарь по умолчанию
+            product_data = {}
 
-            # Извлекаем значения из словаря с использованием значений по умолчанию
         name = product_data.get("name", "")
         description = product_data.get("description", "")
         price = product_data.get("price", 0.0)
         quantity = product_data.get("quantity", 0)
 
-        # Создаем и возвращаем новый экземпляр класса Product
         return cls(name, description, price, quantity)
 
 
