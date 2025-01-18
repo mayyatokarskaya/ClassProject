6921d81cf1d5b6fa09c2c85e7b5d8fbef870e346
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-18 15:26:44 +1000

    create method add_product()

--- src/category.py
+++ src/category.py
@@ -10,6 +10,12 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        """метод добавления товаров в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
