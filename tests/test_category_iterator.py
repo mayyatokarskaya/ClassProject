1da3dcc5fe61a041e958edf38697b9efa60aead9
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-21 12:41:27 +1000

    checking with linters

--- tests/test_category_iterator.py
+++ tests/test_category_iterator.py
@@ -3,6 +3,7 @@ from src.product import Product
 from src.category import Category
 from src.category_iterator import CategoryIterator
 
+
 # Фикстура для создания категории с товарами
 @pytest.fixture
 def sample_category():
@@ -10,10 +11,12 @@ def sample_category():
     product2 = Product("Смартфон", "128GB", 50000.0, 10)
     return Category("Электроника", "Техника для дома", [product1, product2])
 
+
 # Фикстура для создания пустой категории
 @pytest.fixture
 def empty_category():
     return Category("Пустая категория", "Нет товаров", [])
+
 
 # Тест: Итератор корректно перебирает товары
 def test_category_iterator(sample_category):
@@ -27,6 +30,7 @@ def test_category_iterator(sample_category):
     assert products[0].name == "Телевизор"
     assert products[1].name == "Смартфон"
 
+
 # Тест: Итератор завершает итерацию после окончания товаров
 def test_category_iterator_stop_iteration(sample_category):
     iterator = CategoryIterator(sample_category)
@@ -38,6 +42,7 @@ def test_category_iterator_stop_iteration(sample_category):
     # Проверяем, что следующее обращение вызывает StopIteration
     with pytest.raises(StopIteration):
         next(iterator)
+
 
 # Тест: Итератор корректно работает с пустой категорией
 def test_category_iterator_empty_category(empty_category):
