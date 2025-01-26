9679c377969eebf0f99dd1a56665d10223dd7283
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-26 17:25:56 +1000

    create class Ssmartphone()

---
+++ src/smartphone.py
@@ -0,0 +1,18 @@
+from src.product import Product
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+if __name__ == "__main__":
+    product = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "15", "Ifone", "256 ГБ", "grey")
+
+    print(str(product))
+    print(product.description)
+    print(product.efficiency)
