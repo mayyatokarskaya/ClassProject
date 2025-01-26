30d95ced55ad9ad5c5c8f3fb8fa8f32d57c3f00d
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-26 19:04:11 +1000

    add fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 from src.category import Category
 from src.product import Product
+from src.smartphone import Smartphone
 
 
 @pytest.fixture
@@ -49,3 +50,19 @@ def third_category():
 @pytest.fixture
 def product():
     return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def smartphone_valid():
+    """Фикстура для корректного объекта Smartphone."""
+    return Smartphone(
+        name="iPhone 13",
+        description="128GB, Синий",
+        price=70000.0,
+        quantity=10,
+        efficiency=3.2,
+        model="A2634",
+        memory=128,
+        color="Синий",
+    )
+
