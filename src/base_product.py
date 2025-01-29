ca9dbecac84b75018968c3ee024d8e9efddfdb02
Mayya Tokarskaya <mayya.tokarskaya@gmail.com>
2025-01-29 22:31:35 +1000

    create class BaseProduct

---
+++ src/base_product.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class BaseProduct(ABC):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        """Инициализация базового продукта"""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    @abstractmethod
+    def calculate_total_price(self) -> float:
+        """Рассчитывает общую стоимость продукта на складе"""
+        pass
+
+    @classmethod
+    @abstractmethod
+    def new_product(cls, product_data=None):
+        """Создание нового продукта из словаря"""
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        """Возвращает строковое представление продукта"""
+        pass
+
+
